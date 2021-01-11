e796e6db16ca7b6e37c762ddd89b1c65a11cf8e5
RissiHarshan <77276160+RissiHarshan@users.noreply.github.com>
2021-01-11 17:28:46 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="App">
+    <Helloworld msg="hellohi"/>
+    
+    </div>
+</template>
+
+<script>
+  import Helloworld from '@/components/HelloWorld.vue'
+  export default{
+    name:'App',
+    components: {
+      Helloworld
+    },
+  }
+
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>component</h1>
+    <p>{{ msg }}</p>
+    <p>{{ name }}</p>
+    <p>original message:{{ message }}</p>
+    <p>reversed message{{reg}}</p>
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'Helloworld',
+props:{
+  msg:String
+
+},
+data(){
+  return{
+    name:"Rissi Harhan",
+    message:"HelloPeople"
+  }
+},
+methods:{
+reg:function(){
+  return this.message.split('').reverse().join('')
+}
+}
+
+}
+</script>
+
+
+
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
